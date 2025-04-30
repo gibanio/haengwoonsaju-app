7c99bd0b251e0ec7e7856641122ca704c8304c54
서종환 <sjh@giban.io>
2025-04-30 11:14:47 +0900

    [개발] 주소 변경 및 버전 변경 5m

--- constants/Config.ts
+++ constants/Config.ts
@@ -6,7 +6,7 @@ const development = {
 
 const production = {
   IS_DEV_MODE: false,
-  API_HOST: "http://52.79.119.109",
+  API_HOST: "https://dev.haengwoonsaju.com",
   API_SECRET: "8f4e2c1d9b7a3f5e8c2d4b6a9f1e3d5b7c4a2e6f8d0b5a3c7e9f1d4b2a6c8e0",
 };
 
